0e5683d80830b57779c2fac11ef7887b359fcc12
Maxime Dupont <emixamdupont@gmail.com>
2021-06-01 08:57:40 +0200

    Undo failed gh-pages deploy

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ var ghpages = require('gh-pages');
 ghpages.publish(
   'public',
   {
-    branch: 'master',
+    branch: 'gh-pages',
     repo: 'git@github.com:Ennoriel/customizable-library.git',
     user: {
       name: 'Maxime Dupont',
